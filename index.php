1275503f802cf70f8d364249c90115ed31600838
redwanalirafi <redwanalirafi@gmail.com>
2022-04-24 01:14:00 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,327 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Home </title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Dosis&family=Indie+Flower&family=M+PLUS+Code+Latin&family=Open+Sans:wght@300&family=Zen+Kurenaido&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container" style="background-color: azure;  box-shadow: 0 0px 0px 0px #f0f1f5, 0 -9px 0px 0px #f0f1f5, 8px 0 15px -4px black, -12px 0 15px -4px grey;">
+        <nav class="navbar navbar-light bg-dark" style="padding: 0;margin-bottom:10px">
+            <div class="container" >
+                <a class="navbar-brand" href="#">
+                    <img src="image/logo.png" alt="" width="70">
+
+                </a>
+                <span style="color: white; font-size:35px">&nbspWaste Management</span>
+
+
+                <ul class="nav nav-tabs">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" style="color: white">Coverage Area</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" style="color: white">Packages</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" style="color: white">Pay Bill</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" style="color: white">Login</a>
+                    </li>
+                </ul>
+            </div>
+
+        </nav>
+
+        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="image/1.jpg" class="d-block w-100" alt="..." height="600px" width="auto">
+                </div>
+                <div class="carousel-item">
+                    <img src="image/2.jpg" class="d-block w-100" alt="..." height="600px" width="auto">
+                </div>
+                <div class="carousel-item">
+                    <img src="image/3.jpg" class="d-block w-100" alt="..." height="600px" width="auto">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+
+        <div class="jss53">
+
+
+            <div class="row">
+                <div class="col-sm-6 mt-5">
+                    <a href="http://" >
+                        <div class="card" style="width: 30rem; margin:10px auto; box-shadow: 10px 10px 8px #888888;border: 2px solid black;">
+                            <img src="image/card-res.png" class="card-img-top" alt="..." height="300px">
+                            <div class="card-body">
+                                <p class="card-text" style="font-size: 30px;">Residential Waste Pickup</p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-sm-6 mt-5">
+                    <a href="http://" >
+                        <div class="card" style="width: 30rem; margin:10px auto; box-shadow: 10px 10px 8px #888888; border: 2px solid black;">
+                            <img src="image/card-buss.png" class="card-img-top" alt="..." height="300px">
+                            <div class="card-body">
+                                <p class="card-text" style="font-size: 30px;">Bussiness Waste Pickup</p>
+                            </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="card border-dark mb-3 my-5 mb-5" style="padding: 10px auto">
+            <div class="card-body text-dark">
+
+                <div class="contentList" style="padding: 10px auto">
+                    <div class="ButtonRow d-flex">
+                        <div class="button-wrapper"><button class="Button a-taggable btn maax-font-weight-bold rounded-0 d-flex flex-column align-items-center justify-content-center bg-transparent" name="" type="button" analytics="mywm-list-pay-bill" aria-label="" aria-haspopup=""><img src="https://cdn.wm.com/content/dam/wm/icons/branding/30x30/green/laptop.svg" alt="">
+                                <div class="button-text text-center button-icon-text maax-font-weight-bold maax-font-weight-bold wm-text-gray-dark">Pay Your Bill Online</div>
+                            </button></div>
+                        <div class="button-wrapper"><button class="Button a-taggable btn maax-font-weight-bold rounded-0 d-flex flex-column align-items-center justify-content-center bg-transparent" name="" type="button" analytics="mywm-list-paperless" aria-label="" aria-haspopup=""><img src="https://cdn.wm.com/content/dam/wm/icons/utilities/30x30/green/phone-billing.svg" alt="">
+                                <div class="button-text text-center button-icon-text maax-font-weight-bold maax-font-weight-bold wm-text-gray-dark">Get Paperless Billing</div>
+                            </button></div>
+                        <div class="button-wrapper"><button class="Button a-taggable btn maax-font-weight-bold rounded-0 d-flex flex-column align-items-center justify-content-center bg-transparent" name="" type="button" analytics="mywm-list-autopay" aria-label="" aria-haspopup=""><img src="https://cdn.wm.com/content/dam/wm/icons/utilities/30x30/green/automatic-payment.svg" alt="">
+                                <div class="button-text text-center button-icon-text maax-font-weight-bold maax-font-weight-bold wm-text-gray-dark">Enroll in AutoPay</div>
+                            </button></div>
+                        <div class="button-wrapper"><button class="Button a-taggable btn maax-font-weight-bold rounded-0 d-flex flex-column align-items-center justify-content-center bg-transparent" name="" type="button" analytics="mywm-list-schedule" aria-label="" aria-haspopup=""><img src="https://cdn.wm.com/content/dam/wm/icons/utilities/30x30/green/calendar-schedule.svg" alt="">
+                                <div class="button-text text-center button-icon-text maax-font-weight-bold maax-font-weight-bold wm-text-gray-dark">See Pickup Schedules</div>
+                            </button></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+    </div>
+
+
+
+
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+
+    <!-- Footer -->
+    <footer class="bg-dark text-center text-white">
+        <!-- Grid container -->
+        <div class="container p-4">
+            <!-- Section: Social media -->
+            <section class="mb-4">
+                <!-- Facebook -->
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-facebook-f"></i></a>
+
+                <!-- Twitter -->
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-twitter"></i></a>
+
+                <!-- Google -->
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-google"></i></a>
+
+                <!-- Instagram -->
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-instagram"></i></a>
+
+                <!-- Linkedin -->
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-linkedin-in"></i></a>
+
+                <!-- Github -->
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-github"></i></a>
+            </section>
+            <!-- Section: Social media -->
+
+            <!-- Section: Form -->
+            <section class="">
+                <form action="">
+                    <!--Grid row-->
+                    <div class="row d-flex justify-content-center">
+                        <!--Grid column-->
+                        <div class="col-auto">
+                            <p class="pt-2">
+                                <strong>Sign up for our newsletter</strong>
+                            </p>
+                        </div>
+                        <!--Grid column-->
+
+                        <!--Grid column-->
+                        <div class="col-md-5 col-12">
+                            <!-- Email input -->
+                            <div class="form-outline form-white mb-4">
+                                <input type="email" id="form5Example21" class="form-control" />
+                                <label class="form-label" for="form5Example21">Email address</label>
+                            </div>
+                        </div>
+                        <!--Grid column-->
+
+                        <!--Grid column-->
+                        <div class="col-auto">
+                            <!-- Submit button -->
+                            <button type="submit" class="btn btn-outline-light mb-4">
+                                Subscribe
+                            </button>
+                        </div>
+                        <!--Grid column-->
+                    </div>
+                    <!--Grid row-->
+                </form>
+            </section>
+            <!-- Section: Form -->
+
+            <!-- Section: Text -->
+            <section class="mb-4">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt distinctio earum
+                    repellat quaerat voluptatibus placeat nam, commodi optio pariatur est quia magnam
+                    eum harum corrupti dicta, aliquam sequi voluptate quas.
+                </p>
+            </section>
+            <!-- Section: Text -->
+
+            <!-- Section: Links -->
+            <section class="">
+                <!--Grid row-->
+                <div class="row">
+                    <!--Grid column-->
+                    <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                        <h5 class="text-uppercase">Links</h5>
+
+                        <ul class="list-unstyled mb-0">
+                            <li>
+                                <a href="#!" class="text-white">Link 1</a>
+                            </li>
+                            <li>
+                                <a href="#!" class="text-white">Link 2</a>
+                            </li>
+                            <li>
+                                <a href="#!" class="text-white">Link 3</a>
+                            </li>
+                            <li>
+                                <a href="#!" class="text-white">Link 4</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <!--Grid column-->
+
+                    <!--Grid column-->
+                    <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                        <h5 class="text-uppercase">Links</h5>
+
+                        <ul class="list-unstyled mb-0">
+                            <li>
+                                <a href="#!" class="text-white">Link 1</a>
+                            </li>
+                            <li>
+                                <a href="#!" class="text-white">Link 2</a>
+                            </li>
+                            <li>
+                                <a href="#!" class="text-white">Link 3</a>
+                            </li>
+                            <li>
+                                <a href="#!" class="text-white">Link 4</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <!--Grid column-->
+
+                    <!--Grid column-->
+                    <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                        <h5 class="text-uppercase">Links</h5>
+
+                        <ul class="list-unstyled mb-0">
+                            <li>
+                                <a href="#!" class="text-white">Link 1</a>
+                            </li>
+                            <li>
+                                <a href="#!" class="text-white">Link 2</a>
+                            </li>
+                            <li>
+                                <a href="#!" class="text-white">Link 3</a>
+                            </li>
+                            <li>
+                                <a href="#!" class="text-white">Link 4</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <!--Grid column-->
+
+                    <!--Grid column-->
+                    <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                        <h5 class="text-uppercase">Links</h5>
+
+                        <ul class="list-unstyled mb-0">
+                            <li>
+                                <a href="#!" class="text-white">Link 1</a>
+                            </li>
+                            <li>
+                                <a href="#!" class="text-white">Link 2</a>
+                            </li>
+                            <li>
+                                <a href="#!" class="text-white">Link 3</a>
+                            </li>
+                            <li>
+                                <a href="#!" class="text-white">Link 4</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <!--Grid column-->
+                </div>
+                <!--Grid row-->
+            </section>
+            <!-- Section: Links -->
+        </div>
+        <!-- Grid container -->
+
+        <!-- Copyright -->
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+            © 2020 Copyright:
+            <a class="text-white" href="https://mdbootstrap.com/">HogarNati.com</a>
+        </div>
+        <!-- Copyright -->
+    </footer>
+    <!-- Footer -->
+</body>
+
+
+
+</html>
